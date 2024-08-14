07b636380c1abd6cd60739aee996137f4da9f202
youssef emad eldin <youssefemadeldn>
2024-08-14 22:48:15 +0300

    addTask function

---
+++ lib/firebase/firebase_functions.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:todo/model/task_model.dart';
+
+class FirebaseFunctions {
+  static CollectionReference<TaskModel> getTaskCollection() {
+    return FirebaseFirestore.instance
+        .collection("Tasks")
+        .withConverter<TaskModel>(
+          fromFirestore: (snapshot, _) => TaskModel.fromJson(snapshot.data()!),
+          toFirestore: (taskModel, _) => taskModel.toJson(),
+        );
+  }
+
+  static void addTask(TaskModel taskModel) {
+    var collection = getTaskCollection();
+
+    var docRef = collection.doc();
+    taskModel.id = docRef.id;
+    docRef.set(taskModel);
+  }
+}
